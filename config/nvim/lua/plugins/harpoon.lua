43f2d9ac8dcb8778e56730a752521ab47a44e7b2
Ruslan Yakhyaev <ruslan@ruslan.io>
2024-08-25 22:17:05 +0200

    Try out harpoon

---
+++ config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,13 @@
+return {
+  "ThePrimeagen/harpoon",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  "nvim-telescope/telescope.nvim"
+  },
+  config = function()
+    require("telescope").load_extension("harpoon")
+
+    vim.keymap.set("n", "<Leader>hm", "<cmd>lua require('harpoon.mark').add_file()<CR>")
+    vim.keymap.set("n", "<Leader>hl", "<cmd>Telescope harpoon marks<CR>")
+  end
+}
