56590e686b1b6c9f5387e9beee571f6e68a6fd5a
Ruslan Yakhyaev <ruslan@ruslan.io>
2024-08-25 14:26:55 +0200

    Code styling

--- config/nvim/lua/plugins/ufo.lua
+++ config/nvim/lua/plugins/ufo.lua
@@ -1,7 +1,7 @@
 -- Sane folding
 local handler = function(virtText, lnum, endLnum, width, truncate)
   local newVirtText = {}
-  local suffix = (" ····· 󰁂 %d "):format(endLnum - lnum)
+  local suffix = (" ····· ▼ %d "):format(endLnum - lnum)
   local sufWidth = vim.fn.strdisplaywidth(suffix)
   local targetWidth = width - sufWidth
   local curWidth = 0
@@ -24,7 +24,7 @@ local handler = function(virtText, lnum, endLnum, width, truncate)
     curWidth = curWidth + chunkWidth
   end
 
-  table.insert(newVirtText, {suffix, 'MoreMsg'})
+  table.insert(newVirtText, { suffix, "MoreMsg" })
 
   return newVirtText
 end
