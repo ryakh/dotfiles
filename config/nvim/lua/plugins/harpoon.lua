b90e24bec50afd368d2c6076271f87f8a85fb385
Ruslan Yakhyaev <ruslan@ruslan.io>
2024-10-05 09:56:43 +0200

    Alignment

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -2,7 +2,7 @@ return {
   "ThePrimeagen/harpoon",
   dependencies = {
     "nvim-lua/plenary.nvim",
-  "nvim-telescope/telescope.nvim"
+    "nvim-telescope/telescope.nvim"
   },
   config = function()
     require("telescope").load_extension("harpoon")
