267bc96f168dd171b2b84bfbbd9a9f3422196c31
Ruslan Yakhyaev <ruslan@ruslan.io>
2025-01-07 11:11:33 +0100

    Auto-dark mode for vim

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -4,9 +4,20 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      flavour = "mocha"
+      flavour = "auto",
+      background = {
+        dark = "mocha",
+        light = "latte",
+      },
+      integrations = {
+        telescope = true,
+        mason = true,
+        notify = true,
+      },
     })
-
+  end,
+  init = function()
+    vim.o.termguicolors = true
     vim.cmd.colorscheme "catppuccin"
   end
 }
